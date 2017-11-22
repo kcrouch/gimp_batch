7e32100a024c91af36386d4faf482b90b9a3d76f
hat <hat@badseed.local>
2017-11-22 14:01:15 -0500

    Added batch loading to the CarTOONize.

--- cartoon-batch.sh
+++ cartoon-batch.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 GIMP_BIN=/Applications/GIMP.app/Contents/MacOS/GIMP
-$GIMP_BIN -i -f -d --verbose -b "(cartoon_batch \"frame\" \".\")" -b "(gimp-quit 0)"
+#$GIMP_BIN -i -f -d --verbose -b "(cartoon_batch \"frame\" \".\")" -b "(gimp-quit 0)"
+$GIMP_BIN -i -f -d --verbose -b "(script-fu-CarTOONizeCLI \"frame\")" -b "(gimp-quit 0)"
